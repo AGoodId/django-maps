28383afbc57b19a36f65e236e060516b9e9ad3f4
Henrik Almer <henrik@agoodid.se>
2013-07-10 13:11:54 +0200

    Startapp and add setup script

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='django-map',
+      version='0.1',
+      description='Django app that provides a model, field and widget for connecting addresses to Google Map geocoded data',
+      author='AGoodId',
+      author_email='teknik@agoodid.se',
+      url='http://github.com/AGoodId/django-map/',
+      packages=['map',],
+      package_data = {
+          'map': [
+              'static/*',
+              'templates/*.html',
+              'templates/*/*.html',
+          ],
+      },
+      license='BSD',
+      include_package_data = False,
+      zip_safe = False,
+      classifiers = [
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Programming Language :: Python',
+          'Operating System :: OS Independent',
+          'Environment :: Web Environment',
+          'Framework :: Django',
+      ],
+)
