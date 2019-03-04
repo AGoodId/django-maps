3f4e994b95fd09e017f86c786f9d39cbccfdd306
Oscar Fröberg <oscarfroberg@Oscars-MacBook-Air.local>
2019-03-04 16:49:04 +0100

    Added API key to template tag.

--- maps/templatetags/map_tags.py
+++ maps/templatetags/map_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.conf import settings
 
 from maps.models import GMap
 
@@ -7,6 +8,8 @@ register = template.Library()
 
 @register.inclusion_tag('maps/gmap.html')
 def google_map(*addresses, **kwargs):
+  gm_key = getattr(settings, "GMAP_KEY", None)
+  api_key=gm_key
   maps = GMap.objects.filter(address__in=addresses)
   markers = [("%f" % m.latitude, "%f" % m.longitude) for m in maps]
   map_uid = str(abs(hash(addresses)))
